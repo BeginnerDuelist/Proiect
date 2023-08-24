e4c3b037f327bdf8ed76d053bda92adb76d7c9c2
Catalin <catalinbuza31@gmail.com>
2023-08-24 11:01:49 +0300

    Add project files.

---
+++ Proiect/Server/StudentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proiect.Shared;
+
+namespace Proiect.Server
+{
+     public class StudentDbContext:DbContext
+     {
+          public StudentDbContext(DbContextOptions<StudentDbContext> options):base(options) { }
+
+          public DbSet<Students> Students { get; set; }
+     }
+}
